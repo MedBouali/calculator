12fb01a55c46fb7448166c9333cae61f176ea6fc
Mohammed Bouali <68650187+MedBouali@users.noreply.github.com>
2025-01-24 16:15:59 +0100

    Add basic calculator functions: add, subtract, multiply, divide, operate

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+const add = function(a, b) {
+    return a + b;
+}
+
+const subtract = function(a, b) {
+    return a - b;
+}
+
+const multiply = function(a, b) {
+    return a * b;
+}
+
+const divide = function(a, b) {
+    return a / b;
+}
+
+const operate = function(operator, num1, num2) {
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+    switch(operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            return "Invalid operator!";
+    }
+}
