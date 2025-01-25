a24c58af20d7a42243d14aed7bb90d489167e587
Mohammed Bouali <68650187+MedBouali@users.noreply.github.com>
2025-01-25 22:34:50 +0100

    Add functions to handle button clicks for digits and operators

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+let displayContent = "0";
+
+const display = document.querySelector(".display");
+const digitButtons = document.querySelectorAll(".num-btn");
+const operatorButtons = document.querySelectorAll(".ope-btn");
+const equalsButton = document.querySelector(".equ-btn");
+
 const add = function(a, b) {
     return a + b;
 }
@@ -14,11 +24,11 @@ const divide = function(a, b) {
     return a / b;
 }
 
-const operate = function(operator, num1, num2) {
+const operate = function(op, num1, num2) {
     num1 = parseFloat(num1);
     num2 = parseFloat(num2);
 
-    switch(operator) {
+    switch(op) {
         case "+":
             return add(num1, num2);
         case "-":
@@ -31,3 +41,37 @@ const operate = function(operator, num1, num2) {
             return "Invalid operator!";
     }
 }
+
+const onDigitButtonClick = function(digitButtonValue) {
+    if(displayContent === "0") displayContent = "";
+
+    displayContent += digitButtonValue;
+    display.textContent = displayContent;
+}
+
+const onOperatorButtonClick = function(operatorButtonValue) {
+    onEqualsButtonClick();
+
+    operator = operatorButtonValue;
+    firstNumber = display.textContent;
+    displayContent = "0";
+}
+
+const onEqualsButtonClick = function() {
+    if(operator !== "" && displayContent !== "0") {
+        secondNumber = display.textContent;
+        display.textContent = operate(operator, firstNumber, secondNumber);
+        operator = "";
+        displayContent = "0";
+    }
+}
+
+digitButtons.forEach(button => {
+    button.addEventListener("click", () => onDigitButtonClick(button.textContent));
+});
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => onOperatorButtonClick(button.textContent));
+});
+
+equalsButton.addEventListener("click", onEqualsButtonClick);
